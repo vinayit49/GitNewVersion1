be7f9c93e8c3b445b617c5c8252c023337247372
vinay <vinayit716@gmail.com>
2024-01-23 14:38:59 +0530

    Commiting Test3 in BranchX

---
+++ src/Vinay/Test3.java
@@ -0,0 +1,21 @@
+package Vinay;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		String hungry="yes";
+		if(hungry=="yes")
+		{
+			System.out.println("eat something....");
+		}
+		
+			else
+			{
+				System.out.println("Do work....");
+			}
+		}
+		
+
+	}
+
+
